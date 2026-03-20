ff5d5f38d13fa9fc0d0521cb4114c0ba83903b17
wonarii <ariellecwong@gmail.com>
2026-03-19 20:44:32 -0400

    working on camera movement

---
+++ Projects/DuckAndCatV1.0/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    //move zones
+    [SerializeField] GameObject moveZoneR;
+    [SerializeField] GameObject moveZoneL;
+    
+    //camera itself
+    [SerializeField] Camera camera;
+    [SerializeField] private float cameraSpeed;
+    
+    //players
+    [SerializeField] Rigidbody2D duck;
+    [SerializeField] Rigidbody2D cat;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(cat.transform.position.x);
+        //when cat is past the move line, move camera so cat is about 1/4 way on the screen
+        //BUT if duck is too close to the other edge (move zone) no camera movement allowed!!!!
+        
+        //checking the right
+        //if cat is close to right edge and duck is not close to left edge
+        if (cat.transform.position.x > moveZoneR.transform.position.x && duck.transform.position.x > moveZoneL.transform.position.x)
+        {
+            Debug.Log("IN THE RIGHT MOVE ZONE");
+            moveCamera(1);
+        }
+        
+        //checking left
+        //if cat is close to left edge and duck is not close to right edge
+        if (cat.transform.position.x < moveZoneL.transform.position.x && duck.transform.position.x < moveZoneR.transform.position.x)
+        {
+            Debug.Log("IN THE LEFT MOVE ZONE");
+            moveCamera(-1);
+        }
+    }
+
+    private void moveCamera(int direction)
+    {
+        camera.transform.position = new Vector3(camera.transform.position.x + cameraSpeed * direction, camera.transform.position.y, camera.transform.position.z);
+    }
+}
