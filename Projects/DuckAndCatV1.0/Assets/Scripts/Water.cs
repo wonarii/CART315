a105023560073c1c3161af3b7bf21a26c135fc2f
wonarii <ariellecwong@gmail.com>
2026-03-21 20:44:54 -0400

    cat dies in water
    respawns with a delay

---
+++ Projects/DuckAndCatV1.0/Assets/Scripts/Water.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] private GameObject cat;
+    [SerializeField] private GameObject duck;
+    [SerializeField] private GameObject respawnR;
+    [SerializeField] private GameObject respawnL;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "cat")
+        {
+            StartCoroutine(disableCat());
+            
+        }
+    }
+
+    private void respawnCat()
+    {
+        if (Mathf.Abs(duck.transform.position.x - respawnR.transform.position.x) <
+            Mathf.Abs(duck.transform.position.x - respawnL.transform.position.x))
+        {
+            cat.transform.position = respawnR.transform.position; 
+        }
+        else
+        {
+            cat.transform.position = respawnL.transform.position;
+        }
+        
+    }
+
+   
+    IEnumerator disableCat()
+    {
+        
+        cat.GetComponent<SpriteRenderer>().enabled = false;
+        cat.GetComponent<BoxCollider2D>().enabled = false;
+        cat.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        yield return new WaitForSeconds(1);
+        cat.GetComponent<SpriteRenderer>().enabled = true;
+        cat.GetComponent<BoxCollider2D>().enabled = true;
+        respawnCat();
+        yield return new WaitForSeconds(1);
+        cat.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        
+        
+        
+    }
+    
+}
